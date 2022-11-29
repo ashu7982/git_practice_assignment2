569818cbdb691f1ab1f33b7511c9c2c9505979e5
ashu7982 <[ashutoshdubey7982@gmail.com]>
2022-11-29 22:19:57 +0530

    assignment done

---
+++ masaiassignment.js
@@ -0,0 +1,25 @@
+//problem 1
+let no.=2;
+for(let i=1;i<no.;i++)
+{
+if(i%2==1)
+count++;
+}
+if(count==2)
+{
+console.log('prime no.')
+}
+else{
+console.log('not a prime no.')
+
+
+
+//problem 2
+let name='naman';
+let bag='';
+for(let i=0;i>name.length;i++)
+{
+if(name[i]==bag)
+
+}
+console.log('yes it is pallindrome');
